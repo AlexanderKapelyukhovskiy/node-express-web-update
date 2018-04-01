b835b34fa404f245018f68120bd0f622aeb485f7
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-04-01 23:11:29 +0300

    tooling

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules:{
+        // "linebreak-style": 
+        /*eslint linebreak-style: ["error", "unix"]*/
+        /*eslint linebreak-style: ["error", "windows"]*/
+    }
+};
