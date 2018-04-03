c9a1cff5d3208221d48295240ee40a843b21daf2
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-04-03 19:17:43 +0300

    introduce routes and html template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
     "extends": "airbnb-base",
-    rules:{
+    "rules": {
         // "linebreak-style": 
         /*eslint linebreak-style: ["error", "unix"]*/
         /*eslint linebreak-style: ["error", "windows"]*/
+        "comma-dangle": 0
     }
 };
--- app.js
+++ app.js
@@ -7,14 +7,25 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+const navs = [
+  { link: '/books', title: 'Books' },
+  { link: '/authors', title: 'Authors' }];
+
+const booksRouter = require('./src/routes/bookRoutes');
+
+app.use('/books', booksRouter(navs));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index', { navs, title: 'My Library' });
 });
 
 app.listen(port, () => {
